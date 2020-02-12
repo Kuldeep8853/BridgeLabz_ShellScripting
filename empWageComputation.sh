6136c7ca7f019facd3d6f6bc7a104147d0f0fe04
Kuldeep Kasaudhan <59829843+Kuldeep8853@users.noreply.github.com>
2020-02-12 18:17:23 +0530

    [Kuldeep]Add. Solving using Case Statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,31 +1,47 @@
 #!/bin/bash/
-echo "Calculate Daily Employee wage"
-calculateEmpWage(){
-	TotalWage=0
-	WagePerHour=20
-	DailyWage=$(( $WagePerHour*8 ))
-	for(( i=1;i<=20;i++ ))
-	do
-		n=$(( RANDOM%5 ))
-		if(( n>0 ))
-		then
-			TotalWage=$(( $TotalWage+$DailyWage ))
-		fi
-	done
-	echo "$TotalWage "
-}
-fullTime=$( calculateEmpWage )
-echo "Full time work wage of the employee $fullTime$"
+echo "Please Enter the choice: "
+echo "1. Calculate the full time employee total wage..."
+echo "2. Calculate the part time employee total wage..."
+echo "3. Calculate the total wage of employee..."
+read choice
+case $choice in
+1)
+	echo "Calculate Daily Employee wage"
+	calculateEmpWage(){
+		TotalWage=0
+		WagePerHour=20
+		DailyWage=$(( $WagePerHour*8 ))
+		for(( i=1;i<=20;i++ ))
+		do
+			n=$(( RANDOM%5 ))
+			if(( n>0 ))
+			then
+				TotalWage=$(( $TotalWage+$DailyWage ))
+			fi
+		done
+		echo "$TotalWage "
+	}
+	fullTime=$( calculateEmpWage )
+	echo "Full time work wage of the employee $fullTime$"
+;;
 
-echo "Add Part time Employee & wage"
-calculatePartTimeEmpWage(){
-	PerDayHour=4
-	WagePerHour=20
-	EmpWage=$(( $WagePerHour*$PerDayHour ))
+2)
+	echo "Add Part time Employee & wage"
+	calculatePartTimeEmpWage(){
+		PerDayHour=4
+		WagePerHour=20
+		EmpWage=$(( $WagePerHour*$PerDayHour ))
 	echo $EmpWage
-}
-EmpPartTimeWage=$( calculatePartTimeEmpWage )
-echo "Part time working wage of the employee $EmpPartTimeWage $"
-totalWage=$(( $fullTime+$EmpPartTimeWage ))
- echo "Total Employee wage $totalWage $"
+	}
+	EmpPartTimeWage=$( calculatePartTimeEmpWage )
+	echo "Part time working wage of the employee $EmpPartTimeWage $"
+;;
 
+3)
+	totalWage=$(( $fullTime+$EmpPartTimeWage ))
+ 	echo "Total Employee wage $totalWage $"
+;;
+
+*)
+	echo "Enter the valid choice!! "
+ esac
