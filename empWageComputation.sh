436478247a2d6deb479fae20828b740b7e5238c3
Kuldeep Kasaudhan <59829843+Kuldeep8853@users.noreply.github.com>
2020-02-12 13:20:40 +0530

    [Kuldeep]Add. Employee AttendanceFeature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,17 @@
 #!/bin/bash/
-
 echo "Welcome to Employee Wage Computational program on Master Branch"
+echo "Check the employee present or absent"
+checkPresentAbsent(){
+	present=0
+	for(( i=1;i<=20;i++ ))
+	do
+		n=$(( RANDOM%5 ))
+		if(( n>0 ))
+		then
+			present=$(( $present+1 ))
+		fi
+	done
+	echo $present
+}
+present=$( checkPresentAbsent )
+echo "Employee present $present days of 20 office days"
