8dcda8b4c0858d6804b6fcb7466f94c3f7ebccc6
Kuldeep Kasaudhan <59829843+Kuldeep8853@users.noreply.github.com>
2020-02-12 18:29:05 +0530

    [Kuldeep]Add. Solving using Case Statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,7 @@
 #!/bin/bash/
-echo "Please Enter the choice: "
-echo "1. Calculate the full time employee total wage..."
-echo "2. Calculate the part time employee total wage..."
-echo "3. Calculate the total wage of employee..."
-read choice
-case $choice in
-1)
-	echo "Calculate Daily Employee wage"
-	calculateEmpWage(){
+
+echo "Calculate Daily Employee wage"
+calculateEmpWage(){
 		TotalWage=0
 		WagePerHour=20
 		DailyWage=$(( $WagePerHour*8 ))
@@ -20,23 +14,29 @@ case $choice in
 			fi
 		done
 		echo "$TotalWage "
-	}
+}
 	fullTime=$( calculateEmpWage )
-	echo "Full time work wage of the employee $fullTime$"
-;;
 
-2)
-	echo "Add Part time Employee & wage"
-	calculatePartTimeEmpWage(){
+echo "Add Part time Employee & wage"
+calculatePartTimeEmpWage(){
 		PerDayHour=4
 		WagePerHour=20
 		EmpWage=$(( $WagePerHour*$PerDayHour ))
 	echo $EmpWage
-	}
-	EmpPartTimeWage=$( calculatePartTimeEmpWage )
+}
+EmpPartTimeWage=$( calculatePartTimeEmpWage )
+echo "Please Enter the choice: "
+echo "1. Calculate the full time employee total wage..."
+echo "2. Calculate the part time employee total wage..."
+echo "3. Calculate the total wage of employee..."
+read choice
+case $choice in
+1)
+	echo "Full time work wage of the employee $fullTime"
+;;
+2)
 	echo "Part time working wage of the employee $EmpPartTimeWage $"
 ;;
-
 3)
 	totalWage=$(( $fullTime+$EmpPartTimeWage ))
  	echo "Total Employee wage $totalWage $"
