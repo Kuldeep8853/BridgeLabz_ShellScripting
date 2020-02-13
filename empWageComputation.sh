fbe8f1160cdff0dbbb06838f5dc3389fb43dd3c3
Kuldeep Kasaudhan <59829843+Kuldeep8853@users.noreply.github.com>
2020-02-13 10:57:53 +0530

    [Kuldeep]Refactore.Write Fuction to get work hour

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,21 +1,4 @@
 #!/bin/bash/ 
-echo "Welcome to Employee Wage Computation program on Master branch"
-
-echo "Check Employee present or absent"
-checkEmpPresentAbsent(){
-	present=0
-	for(( i=1;i<=20;i++ ))
-	do
-		num=$(( RANDOM%5 ))
-		if(( num>0 ))
-		then
-			present=$(( $present+1 ))
-		fi
-	done
-	echo $present
-}
-present=$( checkEmpPresentAbsent )
-echo "Employee $present days present in 20 office days"
 
 echo "Calculate Daily Employee wage"
 calculateEmpWage(){
