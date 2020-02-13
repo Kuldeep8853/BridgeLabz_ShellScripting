e32218714d11c16ee60726e190feba38bec81fbc
Kuldeep Kasaudhan <59829843+Kuldeep8853@users.noreply.github.com>
2020-02-13 10:54:22 +0530

    [Kuldeep]Add.Write Fuction to get work hour

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -43,27 +43,6 @@ calculatePartTimeEmpWage(){
 }
 EmpPartTimeWage=$( calculatePartTimeEmpWage )
 
-echo "Please Enter the choice: "
-echo "1. Calculate the full time employee total wage..."
-echo "2. Calculate the part time employee total wage..."
-echo "3. Calculate the total wage of employee..."
-read choice
-case $choice in
-1)
-	echo "Full time work wage of the employee $fullTime"
-;;
-2)
-	echo "Part time working wage of the employee $EmpPartTimeWage $"
-;;
-3)
-	totalWage=$(( $fullTime+$EmpPartTimeWage ))
- 	echo "Total Employee wage $totalWage $"
-;;
-
-*)
-	echo "Enter the valid choice!! "
- esac
-
 echo "Please Enter the how many days part time work: "
 read partTime
  for(( i=1;i<=partTime;i++ ))
@@ -73,14 +52,10 @@ read partTime
 totalWageOfMonth=$(( $fullTime+$partTimeWage ))
 echo "Total Wage of the the month is $totalWageOfMonth"
 
-echo "Total working of the month 20 days and 150 hour"
-totalWageOfMonth=$(( 150*20 ))
-if(( $fullTime < $totalWageOfMonth ))
-then
-	RemainingWage=$(( $totalWageOfMonth-$fullTime ))
-fi
-remainingHour=$(( $RemainingWage/20 ))
-echo "Remaining hour of the month $remainingHour and complete remaining hour doing part time work"
-partTimeDays=$(( $remainingHour/4 ))
-echo " do the $partTimeDays days part time and complete the remaining hour" 
-
+echo "Calculate the total working hour in the month"
+checkWorkHour(){
+	totalHour=$(( $1/20 ))
+	echo " $totalHour"
+}
+totalHour=$( checkWorkHour $totalWageOfMonth )
+echo "Total working hour of the month $totalHour hour "
