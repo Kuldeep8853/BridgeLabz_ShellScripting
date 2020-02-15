d789b940fac6049b51506547e7dc49ede37e1f9c
Kuldeep Kasaudhan <59829843+Kuldeep8853@users.noreply.github.com>
2020-02-15 15:12:31 +0530

    [Kuldeep]Add. Regular Expression Program

--- shell_script/Distance.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash/
-echo "Enter the co-ordinats x and y..."
-read x
-read y
-distance=$( echo "$x" "$y" | awk '{print sqrt(($1*$1)+($2*$2))}')
-printf "Distance between origin and co-ordinates x and y = $distance"
--- shell_script/Test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash/
-a=$(echo scale=2;100/3 |bc);echo $a
-num=$(echo "sqrt ( 10 )" | bc -l);
-echo $num
